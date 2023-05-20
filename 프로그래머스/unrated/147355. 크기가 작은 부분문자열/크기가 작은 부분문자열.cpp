4b1070ecccc71e4bb6710d856f4c7c229edbe7c2
우연 <80298207+wooyn730@users.noreply.github.com>
2023-05-20 19:27:27 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
@@ -8,21 +8,16 @@ int solution(string t, string p) {
     long long num1 = 0;
     long long num2 = 0;
     
-    //num2 = stoll(p); //안되는 이유?
-    for (int i=0; i<p.length(); i++)
-    {
-        num2 *= 10;
-        num2 += p[i];
-    }
+    num2 = stoll(p);
     
     for (int i=0; i<=t.length()-p.length(); i++)
     {
-        num1 = 0;
+        string tmp = "";
+        
         for (int j=i; j<i+p.length(); j++)
-        {
-            num1 *= 10;
-            num1 += t[j];
-        }
+            tmp += t[j];
+        
+        num1 = stoll(tmp);
         
         if (num1 <= num2)
             answer++;
