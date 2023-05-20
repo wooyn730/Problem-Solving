709590d3bac901a11e0991c696391e9c4a7c09b7
우연 <80298207+wooyn730@users.noreply.github.com>
2023-05-20 19:59:27 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    
+    while (order > 0)
+    {
+        if (order%10 != 0 && order%10%3 == 0)
+            answer++;
+        order /= 10;
+    }
+    return answer;
+}
