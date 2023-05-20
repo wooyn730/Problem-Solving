081a0509d19176a8f7906564b49be71af38ada60
wooyn730 <80298207+wooyn730@users.noreply.github.com>
2023-05-20 19:33:15 +0900

    크기가 작은 부분문자열2

---
+++ 프로그래머스/lv1/크기가 작은 부분문자열/크기가 작은 부분문자열_2.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string t, string p) {
+    int answer = 0;
+    long long num1 = 0;
+    long long num2 = 0;
+
+    for (int i = 0; i < p.length(); i++)
+    {
+        num2 *= 10;
+        num2 += p[i];
+    }
+
+    for (int i = 0; i <= t.length() - p.length(); i++)
+    {
+        num1 = 0;
+        for (int j = i; j < i + p.length(); j++)
+        {
+            num1 *= 10;
+            num1 += t[j];
+        }
+
+        if (num1 <= num2)
+            answer++;
+    }
+
+    return answer;
+}
