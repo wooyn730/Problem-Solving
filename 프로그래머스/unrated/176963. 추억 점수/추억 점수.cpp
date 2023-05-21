c30d32efd8706c94404c9760f847ee6a22a0b3ad
우연 <80298207+wooyn730@users.noreply.github.com>
2023-05-21 16:26:04 +0900

    [unrated] Title: 추억 점수, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    
+    for (int i=0; i<photo.size(); i++)
+    {
+        answer.push_back(0);
+        for (int j=0; j<photo[i].size(); j++)
+        {
+            for (int k=0; k<name.size(); k++)
+            {
+                if (photo[i][j] == name[k])
+                    answer[i] += yearning[k];
+            }
+            
+        }
+    }
+    
+    return answer;
+}
