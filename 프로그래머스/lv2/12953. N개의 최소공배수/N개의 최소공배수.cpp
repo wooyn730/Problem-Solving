be405526e3df26f07e0118307b3b5775859c3ffe
우연 <80298207+wooyn730@users.noreply.github.com>
2023-05-22 02:30:36 +0900

    [level 2] Title: N개의 최소공배수, Time: 106.86 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> arr) {
+    sort(arr.begin(), arr.end());
+    
+    for (int i=arr[arr.size()-1]; ; i++)
+    {
+        int cnt = 0;
+        for (int j=0; j<arr.size(); j++)
+        {
+            if (i%arr[j] == 0)
+                cnt++;
+        }
+        if (cnt == arr.size())
+            return i;
+    }
+}
