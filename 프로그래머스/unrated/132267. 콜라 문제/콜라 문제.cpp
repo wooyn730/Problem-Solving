6109e07b6070403871e4e6b70b625d6149fe7aea
우연 <80298207+wooyn730@users.noreply.github.com>
2023-05-21 16:17:43 +0900

    [unrated] Title: 콜라 문제, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+
+    while (n>=a)
+    {
+        n -= a;
+        n += b;
+        answer += b;
+    }
+    
+    return answer;
+}
