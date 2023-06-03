1ce41bc93aa0a5289b71dc536bee482a9fb97531
우연 <80298207+wooyn730@users.noreply.github.com>
2023-06-03 16:29:32 +0900

    Update 프로세스.cpp

--- 프로그래머스/lv2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
@@ -39,17 +38,6 @@ int solution(vector<int> priorities, int location) {
     // 우선순위 종류 내림차순으로 정렬
     sort(priority.begin(), priority.end(), compare);
     
-    /*
-    for (int j=0; j<priority.size(); j++)
-        cout << priority[j].first << " " << priority[j].second << endl;
-    
-    
-    while (!process.empty())
-    {
-        //cout << process.front().first << " " << process.front().second <<endl;
-        process.pop();
-    }*/
-        
     // 큐의 front 가장 높은 우선순위라면 pop, 아니라면 pop 후 다시 push
     // pop할 때 answer+1을 해주고, 내가 찾던 프로세스라면 끝내고 리턴
     while (!process.empty())
@@ -59,29 +47,19 @@ int solution(vector<int> priorities, int location) {
         {
             // 프로세스 처리
             answer++;
-            
             if (process.front().first == myP) // 종료
                 return answer;
-
             process.pop();
-            
             
             // 우선순위 처리
             priority[0].second--;
             if (priority[0].second == 0)
                 priority.erase(priority.begin(), priority.begin()+1);
-            
-            
         }
         else
         {
             process.push(process.front());
             process.pop();
         }
-        
-    //    if (tmp == priority[0].first)
-    //        process.pop();
     }
-    
-    return answer;
 }
