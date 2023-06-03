e8a32b8f5e0e459c4344ce9b631218c33372ea0c
우연 <80298207+wooyn730@users.noreply.github.com>
2023-06-03 23:38:26 +0900

    Update 영어 끝말잇기.cpp

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <set>
 #include <algorithm> // find
-#include <iostream>
 
 using namespace std;
 
@@ -16,29 +15,20 @@ vector<int> solution(int n, vector<string> words) {
         if (words[i-1][words[i-1].size()-1] == words[i][0]) // 끝말잇기
         {
             if (w.find(words[i]) == w.end()) // 중복 x
-            {
                 w.insert(words[i]);
-                cout << "a";
-            }
             else // 중복 o
             {
                 answer[0] = i%n+1; // 탈락자 번호
                 answer[1] = i/n+1; // 탈락자의 차례
-                cout << "b";
                 return answer;
             }
         }
         else // 끝말잇기 실패
         {
-            answer[0] = i%n+1; // 탈락자 번호
-            answer[1] = i/n+1; // 탈락자의 차례
-            cout << "c";
+            answer[0] = i%n+1;
+            answer[1] = i/n+1;
             return answer;
         }
     }
-    
-    // for(auto iter = w.begin(); iter != w.end(); iter++)
-    //    cout << *iter << " " ;
-
     return answer;
 }
