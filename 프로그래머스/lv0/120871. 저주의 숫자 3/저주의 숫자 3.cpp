1e4cdecb67e12747e19673d8d940d744cd414603
우연 <80298207+wooyn730@users.noreply.github.com>
2023-06-08 23:30:53 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for (int i=1; i<=n; i++)
+    {
+        answer++;
+        while (answer%3==0 || answer%10==3 || answer/10%10==3)
+            answer++;
+    }
+        
+    return answer;
+}
