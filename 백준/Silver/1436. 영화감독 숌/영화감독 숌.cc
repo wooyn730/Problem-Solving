dbd5a194ba49dd68099f543647368811eddde3b0
우연 <80298207+wooyn730@users.noreply.github.com>
2023-06-17 23:11:52 +0900

    [Silver V] Title: 영화감독 숌, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -6,13 +6,13 @@ int main() {
 	int N;
 	cin >> N;
 	vector<int> end(N+1);
-	int idx = 1;
+	int idx = 0;
 
 	for (int i = 666; ; i++)
 	{
 		if (i % 1000 == 666 || i / 10 % 1000 == 666 || i / 100 % 1000 == 666 || i / 1000 % 1000 == 666)
-			end[idx++] = i;
-		if (idx == N + 1)
+			end[++idx] = i;
+		if (idx == N)
 			break;
 	}
 	cout << end[N];
