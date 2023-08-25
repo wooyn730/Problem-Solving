2e3e213d70634854bf3a6f3657c6d3cf4ad1bd11
우연 <80298207+wooyn730@users.noreply.github.com>
2023-08-26 00:19:50 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    string s1 = to_string(a) + to_string(b);
+    string s2 = to_string(b) + to_string(a);
+    return (stoi(s1) >= stoi(s2) ? stoi(s1) : stoi(s2));
+}
