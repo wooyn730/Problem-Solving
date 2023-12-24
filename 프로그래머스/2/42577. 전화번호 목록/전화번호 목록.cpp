8f550067b5d9e82420fe188a3fb3a4a43c38910f
우연 <80298207+wooyn730@users.noreply.github.com>
2023-12-24 18:06:00 +0900

    Update 전화번호 목록.cpp

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,21 +1,12 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     sort(phone_book.begin(), phone_book.end());
-
     for (int i=0; i<phone_book.size()-1; i++)
-    {
-        for (int j=i+1; j<phone_book.size(); j++)
-        {
-            if (phone_book[i] == phone_book[j].substr(0, phone_book[i].length()))
-                return false;
-            else
-                break;
-        }
-    }
+        if (phone_book[i] == phone_book[i+1].substr(0, phone_book[i].length()))
+            return false;
     return true;
 }
