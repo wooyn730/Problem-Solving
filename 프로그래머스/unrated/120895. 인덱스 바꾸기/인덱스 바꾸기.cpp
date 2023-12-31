000c4f9613ec55a0795caddea45bb353ae327753
우연 <80298207+wooyn730@users.noreply.github.com>
2023-12-31 17:57:32 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.cpp
@@ -0,0 +1,9 @@
+#include <string>
+using namespace std;
+
+string solution(string my_string, int num1, int num2) {
+    char tmp = my_string[num1];
+    my_string[num1] = my_string[num2];
+    my_string[num2] = tmp;
+    return my_string;
+}
