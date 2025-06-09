244adcf3c8097d9cba652b6f144e1d41f4ea8363
우연 <80298207+wooyn730@users.noreply.github.com>
2025-06-10 00:25:41 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cout << "a";
+}
