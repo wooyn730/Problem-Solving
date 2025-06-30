afe4c82d546bfbaaaf46c032007132ac169cb559
우연 <80298207+wooyn730@users.noreply.github.com>
2025-07-01 00:59:33 +0900

    [Silver II] Title: 통계학, Time: 76 ms, Memory: 4004 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int nums[500001];
+int cnt[8001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    double sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> nums[i];
+
+        sum += nums[i];
+        cnt[nums[i] + 4000]++;
+    }
+
+    sort(nums, nums + N);
+
+    int avg = (int)round(sum / N);
+    int mid = nums[N / 2];
+
+    int mx = 0, idx1 = -1, idx2 = -1;
+    for (int i = 0; i < 8001; i++)
+    {
+        if (cnt[i] > mx)
+        {
+            mx = cnt[i];
+            idx1 = i;
+            idx2 = -1;
+        }
+        else if (cnt[i] == mx && idx2 == -1)
+        {
+            idx2 = i;
+        }
+    }
+
+    int frequent = (idx2 == -1 ? idx1 : idx2) - 4000;
+    int range = nums[N - 1] - nums[0];
+    cout << avg << "\n" << mid << "\n" << frequent << "\n" << range;
+
+    return 0;
+}
