b3309b02b072c9751191accae308241fa86b6c90
우연 <80298207+wooyn730@users.noreply.github.com>
2025-07-03 22:08:04 +0900

    [Silver III] Title: 소수 구하기, Time: 216 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,42 +1,35 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
-int M, N;
-bool num[1000001];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-void Eratos(int n)
-{
-	// 나보다 큰 나의 배수들은 싹 지워줌
-	for (int i = n + 1; i <= N; i++)
-	{
-		if (num[i] && i % n == 0)
-			num[i] = false;
-	}
-}
+    int M, N;
+    cin >> M >> N;
 
-int main(void)
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    for (int i = M; i <= N; i++)
+    {
+        if (i == 1)
+            continue;
 
-	cin >> M >> N;
+        bool isPrime = true;
 
-	for (int i = 2; i <= N; i++)
-	{
-		num[i] = true;
-	}
-	
-	for (int i = 2; i <= sqrt(N); i++)
-	{
-		if (num[i])
-			Eratos(i);
-	}
+        for (int j = 2; j <= sqrt(i); j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
 
-	for (int i = M; i <= N; i++)
-	{
-		if (num[i])
-			cout << i << "\n";
-	}
+        if (isPrime)
+            cout << i << "\n";
+    }
+
+    return 0;
 }
