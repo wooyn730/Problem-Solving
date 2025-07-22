992140abd895ae069c127f3aea2a51c2917b8594
우연 <80298207+wooyn730@users.noreply.github.com>
2025-07-22 23:02:01 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,31 +1,29 @@
 #include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int num[10];
+int nums[10];
 
-int main(void)
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	string N;
-	int set = 0;
-	cin >> N;
+    int N;
+    cin >> N;
 
-	for (int i = 0; i < N.length(); i++)
-	{
-		num[N[i] - '0']++;
-	}
-	
-	int six_nine = (num[6] + num[9] + ((num[6]+num[9])%2==0 ? 0 : 1)) / 2;
-	num[6] = six_nine;
-	num[9] = six_nine;
+    while (N > 0)
+    {
+        nums[N % 10]++;
+        N /= 10;
+    }
 
-	for (int i = 0; i <= 9; i++)
-	{
-		if (num[i] > set)
-			set = num[i];
-	}
+    int sum69 = nums[6] + nums[9];
+    nums[6] = sum69 / 2;
+    nums[9] = sum69 / 2 + (sum69 % 2 == 0 ? 0 : 1);
 
-	cout << set;
+    cout << *max_element(nums, nums + 10);
+    return 0;
 }
